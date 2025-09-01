624eff0dc325d9ca51f214836ae815cfb1b538ad
NEBULA-33 <smltanriverdi02@gmail.com>
2025-09-01 22:58:00 +0300

    devam
    son duzeltmelre ınsallah

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Buraya gelecekte Next.js ile ilgili özel ayarlarını ekleyebilirsin.
+  // Şimdilik boş kalması yeterli.
+};
+
+export default nextConfig;
--- next.config.js/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"PO1+uHkvdLsGzGILcCBhhICnN8phiJHVR9eVPdzYOxA=\"\n}"
